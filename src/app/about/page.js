6dbb2a174ece23978f81d7b678671eb7cb7716be
Ben Linton <benklinton@gmail.com>
2025-01-20 17:47:29 -0700

    set up and started project

---
+++ src/app/about/page.js
@@ -0,0 +1,14 @@
+import RootLayout from '@/components/Layout';
+
+export default function About() {
+    return (
+        <RootLayout>
+        <div>
+            <div class="container mx-auto">
+                <h1 class="text-4xl text-center mt-8">About Us</h1>
+                <p class="text-center mt-4">We are a small team of developers who love snacks!</p>
+            </div>
+        </div>
+        </RootLayout>
+    );
+};
---
+++ src/components/Layout.js
@@ -0,0 +1,12 @@
+import Navbar from './Navbar';
+
+const RootLayout = ({ children }) => {
+    return (
+        <>
+        <Navbar />
+        <main>{children}</main>
+        </>
+    );
+    }
+
+export default RootLayout;
