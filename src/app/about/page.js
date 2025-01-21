0a72615008df277ed2dafc9eb226b2d47558fe88
Ben Linton <benklinton@gmail.com>
2025-01-20 18:11:55 -0700

    removed layout file

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,14 +1,12 @@
-import RootLayout from '@/components/Layout';
-
+import Navbar from '@/components/navbar';
 export default function About() {
     return (
-        <RootLayout>
         <div>
-            <div class="container mx-auto">
-                <h1 class="text-4xl text-center mt-8">About Us</h1>
-                <p class="text-center mt-4">We are a small team of developers who love snacks!</p>
+            <Navbar />
+            <div className="container mx-auto">
+                <h1 className="text-4xl text-center mt-8">About Us</h1>
+                <p className="text-center mt-4">We are a small team of developers who love snacks!</p>
             </div>
         </div>
-        </RootLayout>
     );
 };
--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,13 @@
-import RootLayout from "../components/Layout";
+import Navbar from '@/components/navbar';
+
 export default function Home() {
   return (
-    <RootLayout>
     <div>
-      <div class="container mx-auto">
-        <h1 class="text-4xl text-center mt-8">Welcome to Snack Dash!</h1>
-        <p class="text-center mt-4">The best place to find your favorite snacks!</p>
+      <Navbar />
+      <div className="container mx-auto">
+        <h1 className="text-4xl text-center mt-8">Welcome to Snack Dash!</h1>
+        <p className="text-center mt-4">The best place to find your favorite snacks!</p>
       </div>
     </div>
-    </RootLayout>
   );
 }
